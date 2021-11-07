99255ff93a95c4c4f6fd80186b4734eb21b7a8d7
AnnVarlamova <74834132+AnnVarlamova@users.noreply.github.com>
2021-11-08 00:29:52 +0300

    Add files via upload

---
+++ lab2/child.cpp
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <vector>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    char err = '1';
+    int myfile = open(argv[1], O_CREAT | O_WRONLY, S_IRWXU);
+
+    if (myfile == -1) {
+        err = '2';
+        std::cout << err;
+        fflush(stdout);
+        _exit(EXIT_FAILURE);
+    }
+    
+    float x, res;
+    int n, i;
+    while (std::cin >> n) {
+        for (i = 0; i < n; ++i) {
+            std::cin >> x;
+            if (i == 0) {
+                res = x;
+            } else {
+                if (x == 0) {
+                    err = '0';
+                    std::cout << err;
+                    fflush(stdout);
+                    close(myfile);
+                    _exit(EXIT_FAILURE);
+                } else {
+                    if (i > 0) {
+                        res /= x;
+                    }
+                }
+            }
+        }
+        dprintf(myfile, "%f\n", res);
+        fflush(stdout);
+        std::cout << err;
+        fflush(stdout);
+    }
+    close(myfile);
+    return 0;
+}
---
+++ lab2/parent.cpp
@@ -0,0 +1,100 @@
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <vector>
+#include <iostream>
+
+int main(void) {
+    char *filename = NULL;
+    size_t len = 0;
+    std::cout << "Enter the name of file for answers: ";
+    fflush(stdout);
+    if (getline(&filename, &len, stdin) == -1) {
+        perror("getline");
+        _exit(EXIT_FAILURE);
+    }
+    filename[strlen(filename) - 1] = '\0';
+
+    std::cout << "Enter numerous:\n" << std::endl;
+
+    int fd1[2], fd2[2];
+    if ((pipe(fd1) == -1) || (pipe(fd2) == -1)){
+        perror("pipe");
+        _exit(EXIT_FAILURE);
+    }
+
+    int old_stdout = dup(fileno(stdout));
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        _exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {
+        close(fd2[0]);
+        close(fd1[1]);
+
+        if (dup2(fd1[0], fileno(stdin)) == -1) {
+            perror("child, dup2, stdin");
+            _exit(EXIT_FAILURE);
+        }
+
+        if (dup2(fd2[1], fileno(stdout)) == -1) {
+            perror("child, dup2, stdout");
+            _exit(EXIT_FAILURE);
+        }
+
+        if (execl("child.out", "child.out", filename, NULL) == -1) {
+            perror("execl");
+            _exit(EXIT_FAILURE);
+        }
+    }
+    close(fd1[0]);
+    
+    if (pid > 0) {
+        close(fd1[0]);
+        close(fd2[1]);
+        if (dup2(fd1[1], fileno(stdout)) == -1) {
+            perror("parent, dup2, stdout");
+            _exit(EXIT_FAILURE);
+        }
+        char err = '1', c;
+        float x;
+        std::vector<float> vec;
+        while (scanf("%f%c", &x, &c) > 0) {
+            vec.push_back(x);
+            if (c == '\n') {
+                int n = vec.size();
+                std::cout << n << " ";
+                for (int i = 0; i < n; ++i) {
+                    std::cout << vec[i] << " ";
+                }
+                vec.clear();
+                fflush(stdout);
+                read(fd2[0], &err, sizeof(1));
+                if (err == '0') {
+                    close(fd2[0]);
+                    close(fd1[1]);
+                    dup2(old_stdout, fileno(stdout));
+                    close(old_stdout);
+                    std::cout << "Divizion by zero" << std::endl;
+                    fflush(stdout);
+                    _exit(EXIT_FAILURE);
+                }
+                if (err == '2') {
+                    close(fd2[0]);
+                    close(fd1[1]);
+                    dup2(old_stdout, fileno(stdout));
+                    close(old_stdout);
+                    std::cout << "Opening file" << std::endl;
+                    fflush(stdout);
+                    _exit(EXIT_FAILURE);
+                }
+            }
+        }
+        close(fd1[1]);
+        close(fd2[0]);
+    }
+    return 0;
+}
